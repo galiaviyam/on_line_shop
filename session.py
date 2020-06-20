92815904dd499dc9fd9046a0c7557bd122de5ede
galiaviyam <63307949+galiaviyam@users.noreply.github.com>
2020-06-20 17:23:36 +0300

    Delete session.py

--- session.py
+++
@@ -1,8 +0,0 @@
-class Session(object):
-    def __init__(self, session_id=None, timeout=None, email=None, db=None):
-        self.id = session_id
-        self.timeout = timeout
-        self.email = email
-        self.db = db
-        if self.timeout is None:
-            self.timeout = 3600
